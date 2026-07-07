59d0c31f9c393c6c68f6838ac2acd29070f1dc24
pistachiocloudkingheyTJ <tjcha1213@gmail.com>
2026-07-06 21:40:11 -0400

    Increase default map zoom level when search mode opens

--- src-mvp3/components/ListingMap.tsx
+++ src-mvp3/components/ListingMap.tsx
@@ -71,7 +71,7 @@ export default function ListingMap({ listings, selectedId, onSelect, bottomInset
       map.fitBounds(bounds, {
         paddingTopLeft: [40, 40],
         paddingBottomRight: [40, 40],
-        maxZoom: sheetMode === 'peek' ? 15 : 14,
+        maxZoom: sheetMode === 'peek' ? 16 : 15,
       });
     }
 
@@ -109,7 +109,7 @@ export default function ListingMap({ listings, selectedId, onSelect, bottomInset
       map.fitBounds(bounds, {
         paddingTopLeft: [48, 48],
         paddingBottomRight: [48, 48],
-        maxZoom: 14,
+        maxZoom: 15,
       });
     }
   }, [listings, selectedId, sheetMode, topInset, bottomInset]);
