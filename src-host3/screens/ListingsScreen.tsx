bf0371c469e5fd18b707f656e5a80ddf5fa827ba
pistachiocloudkingheyTJ <tjcha1213@gmail.com>
2026-07-21 03:51:55 -0400

    Fix host demo links and host3 add label

--- src-host3/screens/ListingsScreen.tsx
+++ src-host3/screens/ListingsScreen.tsx
@@ -294,7 +294,7 @@ export default function ListingsScreen({
                 <line x1="12" y1="5" x2="12" y2="19" />
                 <line x1="5" y1="12" x2="19" y2="12" />
               </svg>
-              <span>+ New listing</span>
+              <span>New listing</span>
             </button>
           )}
         </div>
